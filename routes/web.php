1407ccf25846589e78891d09b3e27324b19e0654
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-07-25 22:11:44 +0800

    Updated until Generate Factory and Create Seed Data in Codeholic CC

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,9 @@ Route::get('/', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+Route::get('/welcome', [WelcomeController::class,'welcome'])->name('welcome'); // Using Controller [Controller::class, 'function'] the ->name('name') is to set name?
